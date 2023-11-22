d80ed4a325c2dae96ed50314ee5c07454d9b1724
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-22 20:50:10 +0900

    login path

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       {/* <Route path="/*" element={<CoreApp />} /> */}
 
-      <Route path="/test" element={<LoginPage />} />
+      <Route path="login" element={<LoginPage />} />
 
       <Route path="/" element={<MainPage />} />
     </Routes>
