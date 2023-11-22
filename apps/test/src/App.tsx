efa3a280a8770fda1458bf06648cd880d10b1b13
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-22 20:43:51 +0900

    remove home

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login";
 import MainPage from "./pages/main";
 
-const CoreApp = React.lazy(() => import("@admin/core"));
+// const CoreApp = React.lazy(() => import("@admin/core"));
 
 //v6에서는 exact -> *
 function App() {
   return (
     <Routes>
-      <Route path="/*" element={<CoreApp />} />
+      {/* <Route path="/*" element={<CoreApp />} /> */}
 
       <Route path="/test" element={<LoginPage />} />
 
