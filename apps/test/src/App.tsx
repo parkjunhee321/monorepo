004b4d2da175b2bd052ff966b6735fec3a1e488c
parkjunhee321 <jhee.park@moyoplan.com>
2023-11-24 15:35:54 +0900

    completed

--- apps/test/src/App.tsx
+++ apps/test/src/App.tsx
@@ -5,15 +5,29 @@ import MainPage from "./pages/main";
 
 const CoreApp = React.lazy(() => import("@admin/core"));
 
+function DefaultFallback({ children }: { children: React.ReactNode }) {
+  return (
+    <React.Suspense fallback={<div>loading</div>}>{children}</React.Suspense>
+  );
+}
+
 //v6에서는 exact -> *
 function App() {
   return (
     <Routes>
-      <Route path="/*" element={<CoreApp />} />
+      <Route
+        path="/*"
+        element={
+          <DefaultFallback>
+            <CoreApp />
+          </DefaultFallback>
+        }
+      />
 
       <Route path="/login" element={<LoginPage />} />
 
       <Route path="/" element={<MainPage />} />
+      <Route path="/*" element={<h2>404</h2>} />
     </Routes>
   );
 }
--- apps/web/src/pages/Home/index.tsx
+++ apps/web/src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   console.log(1);
-  return <div style={{ color: "#000" }}>홈이지롱</div>;
+  return <div style={{ color: "#fff" }}>홈이지롱</div>;
 }
 
 export default Home;
